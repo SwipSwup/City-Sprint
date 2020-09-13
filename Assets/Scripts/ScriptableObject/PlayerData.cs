1bb6fbe98ca2d7196ff6bf724875071755fba7a0
SwipSwup <davidveigel.business@gmail.com>
2020-09-13 15:30:11 +0200

    Finished protoype of main menu

---
+++ Assets/Scripts/ScriptableObject/PlayerData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new PlayerData", menuName = "ScriptableObjects/PlayerData")]
+public class PlayerData : ScriptableObject
+{
+    public int highscore;
+    public int coins;
+}
--- Assets/Scripts/UI/MainMenuUIHandler.cs
+++ Assets/Scripts/UI/MainMenuUIHandler.cs
@@ -3,26 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System.IO.IsolatedStorage;
+using UnityEngine.UI;
 
 public class MainMenuUIHandler : MonoBehaviour
 {
     public int maxTick = 3;
     private int tickCounter;
 
+    public PlayerData playerData;
+    public GameObject dataFrame;
+
     public GameObject playButtonObject;
-    public TextMeshProUGUI playButtonText;
+    public GameObject playButtonTextObject;
+    private TextMeshProUGUI playButtonText;
 
     public GameObject upgradeButtonObject;
-    public GameObject upTextObject;
+    public GameObject upgradeButtonTextObject;
 
+    public TextMeshProUGUI highscoreText;
+    public TextMeshProUGUI coinsText;
 
-    private void OnEnable()
-    {
-    }
 
     private void Start()
     {
         GameManager.OnTick += AnimatePlayText;
+        LoadPlayerData();
+        playButtonText = playButtonTextObject.GetComponent<TextMeshProUGUI>();
     }
 
     private int animationIndex;
@@ -38,11 +44,51 @@ public class MainMenuUIHandler : MonoBehaviour
         }
     }
 
+    private void LoadPlayerData()
+    {
+        ChangeHighscore(playerData.highscore.ToString());
+        ChangeCoins(playerData.coins.ToString());
+
+    }
+
+    private void ChangeHighscore(string highscore)
+    {
+        highscoreText.text = highscore;
+    }
+    
+    private void ChangeCoins(string money)
+    {
+        coinsText.text = money;
+    }
+
+    private void DisablePlayButton()
+    {
+        playButtonObject.GetComponent<Image>().enabled = false;
+        playButtonObject.GetComponent<Button>().enabled = false;
+
+        playButtonText.enabled = false;
+    }
+
+    private void disableUpgradeButton()
+    {
+        upgradeButtonObject.GetComponent<Image>().enabled = false;
+        upgradeButtonObject.GetComponent<Button>().enabled = false;
+
+        upgradeButtonTextObject.GetComponent<TextMeshProUGUI>().enabled = false;
+    }
+
+    private void DisableDataFrame()
+    {
+        dataFrame.SetActive(false);
+    }
+
+
     public void OpenUpgradeMenu()
     {
-        LeanTween.scale(upgradeButtonObject, Vector3.one * 50, .5f);
-        playButtonObject.active = false;
-        upTextObject.active = false;
+        DisablePlayButton();
+        DisableDataFrame();
+        upgradeButtonTextObject.GetComponent<TextMeshProUGUI>().enabled = false;
+        LeanTween.scale(upgradeButtonObject, Vector3.one * 50, 1f);
     }
 
 }
