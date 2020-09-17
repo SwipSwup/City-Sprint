5839eb8074f90d397b8b7e2f047128249186b5d0
SwipSwup <davidveigel.business@gmail.com>
2020-09-17 21:04:14 +0200

    GamManger handles coin and game over event

---
+++ Assets/Scripts/Track/ObstacelMixer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacelMixer : MonoBehaviour
+{
+    [SerializeField] private GameObject[] obstacleVariants;
+
+    private void SpawnObstacle() => Instantiate(obstacleVariants[(int)Random.Range(0f, obstacleVariants.Length)], transform);
+    private void Awake() => SpawnObstacle();
+}
