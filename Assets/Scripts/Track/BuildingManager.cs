942d826af9d2a18324e7093ceff5467b1adb11a9
SwipSwup <davidveigel.business@gmail.com>
2020-12-16 09:52:44 +0100

    Added pause feature

---
+++ Assets/Scripts/Track/BuildingManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingManager : MonoBehaviour
+{
+    public GameObject[] buildingPrefabs;
+
+    [SerializeField] private int maxBuildings = 10;
+    [SerializeField] private Transform leftBuildingSpawnLoc;
+    [SerializeField] private Transform rightBuildingSpawnLoc;
+
+    private float speed;
+
+    void Start()
+    {
+        
+    }
+
+    private void SubscribeToEvents()
+    {
+    }
+
+    private void UnsubscribeToEvents()
+    {
+
+    }
+
+
+    private void SpawnRandomBuildingLeft()
+    {
+        GameObject newBuilding = Instantiate(buildingPrefabs[(int)UnityEngine.Random.Range(0f, buildingPrefabs.Length)], leftBuildingSpawnLoc.position, leftBuildingSpawnLoc.rotation);
+
+        Tile tile = newBuilding.AddComponent<Tile>();
+        tile.tileSpeed = speed;
+        tile.isSpacer = true;
+    }
+
+    private void SpawnRandomBuildingRight()
+    {
+        GameObject newBuilding = Instantiate(buildingPrefabs[(int)UnityEngine.Random.Range(0f, buildingPrefabs.Length)], rightBuildingSpawnLoc.position, rightBuildingSpawnLoc.rotation);
+
+        Tile tile = newBuilding.AddComponent<Tile>();
+        tile.tileSpeed = speed;
+        tile.isSpacer = true;
+    }
+}
